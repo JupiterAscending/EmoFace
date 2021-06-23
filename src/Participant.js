97b17ace5b788ff459324ee4f0e13e739ebfc101
Yuriko Hijikata <akanesasu3@gmail.com>
2021-06-23 23:30:54 +0900

    saving score to database"

--- src/Participant.js
+++ src/Participant.js
@@ -3,108 +3,162 @@ import Track from "./Track";
 import React, { useState, useEffect } from "react";
 import * as canvas from "canvas";
 import * as faceapi from "face-api.js";
+import { database } from "./firebase";
 
-export default function Participant ({
-	participant,
-	localParticipant,
-	isAnalysed1,
-	setAnalysed1,
-	isAnalysed2,
-	setAnalysed2,
-	prompt,
-	count,
-	setCount,
+export default function Participant({
+  participant,
+  localParticipant,
+  isAnalysed1,
+  setAnalysed1,
+  isAnalysed2,
+  setAnalysed2,
 }) {
-	const existingPublications = Array.from(participant.tracks.values());
-	const existingTracks = existingPublications.map((publication) => publication.track);
-	const nonNullTracks = existingTracks.filter((track) => track !== null);
+  const existingPublications = Array.from(participant.tracks.values());
+  const existingTracks = existingPublications.map((publication) => publication.track);
+  const nonNullTracks = existingTracks.filter((track) => track !== null);
 
-	const [ tracks, setTracks ] = useState(nonNullTracks);
-	const [ video, setVideo ] = useState("");
-	const [ isReady, setReady ] = useState(false);
-	const [ isCapture, setCapture ] = useState(false);
-	const [ result1, setResult_p1 ] = useState();
-	const [ result2, setResult_p2 ] = useState();
-	const [ canvas, setCanvas ] = useState();
+  const [tracks, setTracks] = useState(nonNullTracks);
+  const [video, setVideo] = useState("");
+  const [isReady, setReady] = useState(false);
+  const [isCapture, setCapture] = useState(false);
+  const [result1, setResult_p1] = useState([
+    {
+      expressions: {
+        angry: 0,
+        disgusted: 0,
+        fearful: 0,
+        happy: 0,
+        neutral: 0,
+        sad: 0,
+        surprised: 0,
+      },
+    },
+  ]);
 
-	useEffect(() => {
-		if (!localParticipant) {
-			participant.on("trackSubscribed", (track) => addTrack(track));
-		}
-	}, []);
+  const [result2, setResult_p2] = useState([
+    {
+      expressions: {
+        angry: 0,
+        disgusted: 0,
+        fearful: 0,
+        happy: 0,
+        neutral: 0,
+        sad: 0,
+        surprised: 0,
+      },
+    },
+  ]);
 
-	function addTrack (track) {
-		setTracks([ ...tracks, track ]);
-	}
+  console.log(result1);
+  const [canvas, setCanvas] = useState();
 
-	function videoCapture () {
-		const name = participant.identity;
-		setCanvas(document.getElementById(name + "-canvas"));
-		setVideo(document.getElementById(name));
-		setReady(true);
-		setCount(count + 1);
+  useEffect(() => {
+    if (!localParticipant) {
+      participant.on("trackSubscribed", (track) => addTrack(track));
+    }
+  }, []);
 
-	}
-	function capture () {
-		let canvasSizeX = 100; //canvasの幅
-		let canvasSizeY = 100; //canvasの高さ
-		canvas.getContext("2d").drawImage(video, 0, 0, canvasSizeX, canvasSizeY); //videoタグの「今」の状態をcanvasに描写
-		setCapture(true);
-	}
+  function addTrack(track) {
+    setTracks([...tracks, track]);
+  }
 
-	async function analyse () {
-		await faceapi.nets.tinyFaceDetector.load("/models");
-		await faceapi.nets.faceExpressionNet.load("/models");
-		const detectionsWithExpressions = await faceapi
-			.detectAllFaces(canvas, new faceapi.TinyFaceDetectorOptions())
-			.withFaceExpressions();
+  function videoCapture() {
+    const name = participant.identity;
+    setCanvas(document.getElementById(name + "-canvas"));
+    setVideo(document.getElementById(name));
+    setReady(true);
+  }
+  function capture() {
+    let canvasSizeX = 300; //canvasの幅
+    let canvasSizeY = (canvasSizeX * video.videoHeight) / video.videoWidth; //canvasの高さ
+    canvas.getContext("2d").drawImage(video, 0, 0, canvasSizeX, canvasSizeY); //videoタグの「今」の状態をcanvasに描写
+    setCapture(true);
+  }
 
-		console.log(detectionsWithExpressions);
-		// console.log(prompt);
+  async function analyse() {
+    await faceapi.nets.tinyFaceDetector.load("/models");
+    await faceapi.nets.faceExpressionNet.load("/models");
+    const detectionsWithExpressions = await faceapi
+      .detectAllFaces(canvas, new faceapi.TinyFaceDetectorOptions())
+      .withFaceExpressions();
 
-		if (isAnalysed1 === false && detectionsWithExpressions.length !== 0) {
-			console.log("setting to result1");
-			setAnalysed1(true);
-			setResult_p1(detectionsWithExpressions);
-		} else if (isAnalysed2 === false && detectionsWithExpressions.length !== 0) {
-			console.log("setting to result2");
-			setAnalysed2(true);
-			setResult_p2(detectionsWithExpressions);
-		}
-	}
+    console.log(detectionsWithExpressions);
 
-	console.log(canvas, "this is canvas");
+    if (isAnalysed1 === false) {
+      console.log("setting to result1");
+      setAnalysed1(true);
+      setResult_p1(detectionsWithExpressions);
+      const score = Math.round(
+        parseFloat(detectionsWithExpressions[0].expressions.happy)
+      );
+      database.scores.doc(room.name).set({
+        [participant.identity]: [score],
+      });
+      database.scores
+        .doc(roomName)
+        .set(
+          {
+            [identity]: 0,
+            finished: false,
+            prompt: "",
+          },
+          { merge: true }
+        )
+        .then(() => {
+          console.log("insertion successful");
+        });
+      //firebaseにスコア投げる
+      // detectionsWithExpressions[0].expressions.happy
+      //float & round 小数点なし　* 100
+    } else {
+      console.log("setting to result2");
+      setAnalysed2(true);
+      setResult_p2(detectionsWithExpressions);
+      //firebaseにスコア投げる
+      // detectionsWithExpressions[0].expressions.happy
+      //float & round 小数点なし　* 100
+    }
+  }
 
-	return (
-		<div className="video-container">
-			<div>
-				{isAnalysed1 === true ? "true" : "false"}
-				{isAnalysed2 === true ? "true" : "false"}
-				{count}
+  console.log(canvas, "this is canvas");
 
-				{tracks.map((track) => (
-					<div>
-						<canvas width="150" id={participant.identity + "-canvas"} />
-					</div>
-				))}
+  return (
+    <div>
+      <div>
+        {isAnalysed1 === true ? "true" : "false"}
+        {isAnalysed2 === true ? "true" : "false"}
 
-				<button onClick={videoCapture}>Set</button>
-				{isReady === true ? <button onClick={capture}>Capture Me</button> : <p>Please click the Set Button</p>}
-				{isCapture === true ? (
-					<button id={participant.identity + "-analyse"} onClick={analyse}>
-						Analyse Me
-					</button>
-				) : (
-					<p>Please click the Set Button</p>
-				)}
-			</div>
-			<div width="100px" className="participant" id={participant.identity + "div"}>
-				<div className="identity">{participant.identity}</div>
-				{tracks.map((track) => <Track key={track} participant={participant} track={track} />)}
-			</div>
-			<br />
-			<p>{result1 ? result1[0].expressions.happy : "nothing"}</p>
-			<p>{result2 ? result2[0].expressions.happy : "nothing"}</p>
-		</div>
-	);
+        {tracks.map((track) => (
+          <div>
+            <canvas id={participant.identity + "-canvas"} />
+          </div>
+        ))}
+
+        <button onClick={videoCapture}>Set</button>
+        {isReady === true ? (
+          <button onClick={capture}>Capture Me</button>
+        ) : (
+          <p>Please click the Set Button</p>
+        )}
+        {isCapture === true ? (
+          <button id={participant.identity + "-analyse"} onClick={analyse}>
+            Analyse Me
+          </button>
+        ) : (
+          <p>Please click the Set Button</p>
+        )}
+      </div>
+      <div className="participant" id={participant.identity + "div"}>
+        <div className="identity">{participant.identity}</div>
+        {tracks.map((track) => (
+          <Track key={track} participant={participant} track={track} />
+        ))}
+      </div>
+
+      {result1[0].expressions ? result1[0].expressions.happy : "nothing"}
+      {result2[0].expressions ? result2[0].expressions.happy : "nothing"}
+    </div>
+  );
 }
+
+// realtime data入れるぜ
--- src/Room.js
+++ src/Room.js
@@ -3,72 +3,80 @@ import Participant from "./Participant";
 // import { RemoteParticipant } from "twilio-video";
 // import "./App.scss";
 
-export default function Room ({ room, returnToLobby }) {
-	const [ isAnalysed1, setAnalysed1 ] = useState(false);
-	const [ isAnalysed2, setAnalysed2 ] = useState(false);
-	const [ prompt, setPrompt ] = useState("happy");
-	const [ count, setCount ] = useState(0);
-	const [ remoteParticipants, setRemoteParticipants ] = useState(Array.from(room.participants.values()));
-	useEffect(() => {
-		room.on("participantConnected", (participant) => addParticipant(participant));
-		room.on("participantDisconnected", (participant) => removeParticipant(participant));
+export default function Room({ room, returnToLobby }) {
+  const [isAnalysed1, setAnalysed1] = useState(false);
+  const [isAnalysed2, setAnalysed2] = useState(false);
+  const [prompt, setPrompt] = useState("happy");
+  const [count, setCount] = useState(0);
+  const [remoteParticipants, setRemoteParticipants] = useState(
+    Array.from(room.participants.values())
+  );
+  useEffect(() => {
+    console.log("this is room in Room component", room);
+    room.on("participantConnected", (participant) => addParticipant(participant));
+    room.on("participantDisconnected", (participant) => removeParticipant(participant));
 
-		window.addEventListener("beforeunload", leaveRoom);
+    window.addEventListener("beforeunload", leaveRoom);
 
-		return () => {
-			leaveRoom();
-		};
-	}, []);
+    return () => {
+      leaveRoom();
+    };
+  }, []);
 
-	function addParticipant (participant) {
-		console.log(`${participant.identity} has joined the room.`);
+  // useEffect count が変わったとき動作、 if count === 2 firebaseにprompt を入れる。
+  // realtimeでprompt表示
 
-		setRemoteParticipants([ ...remoteParticipants, participant ]);
-	}
+  function addParticipant(participant) {
+    console.log(`${participant.identity} has joined the room.`);
 
-	function removeParticipant (participant) {
-		console.log(`${participant.identity} has left the room`);
-		setRemoteParticipants(remoteParticipants.filter((p) => p.identity !== participant.identity));
-	}
-	function leaveRoom () {
-		room.disconnect();
-		returnToLobby();
-	}
+    setRemoteParticipants([...remoteParticipants, participant]);
+  }
 
-	return (
-		<div className="room">
-			You are in ROOM: {room.name}
-			<div className="participants">
-				{count === 2 ? <p id="prompt">{prompt}</p> : ""}
+  function removeParticipant(participant) {
+    console.log(`${participant.identity} has left the room`);
+    setRemoteParticipants(
+      remoteParticipants.filter((p) => p.identity !== participant.identity)
+    );
+  }
+  function leaveRoom() {
+    room.disconnect();
+    returnToLobby();
+  }
 
-				<Participant
-					isAnalysed1={isAnalysed1}
-					setAnalysed1={setAnalysed1}
-					isAnalysed2={isAnalysed2}
-					setAnalysed2={setAnalysed2}
-					key={room.localParticipant.identity}
-					localParticipant="true"
-					participant={room.localParticipant}
-					prompt={prompt}
-					count={count}
-					setCount={setCount}
-				/>
-				{remoteParticipants.map((participant) => (
-					<Participant
-						key={participant.identity}
-						participant={participant}
-						isAnalysed1={isAnalysed1}
-						setAnalysed1={setAnalysed1}
-						isAnalysed2={isAnalysed2}
-						setAnalysed2={setAnalysed2}
-						count={count}
-						setCount={setCount}
-					/>
-				))}
-			</div>
-			<button id="leaveRoom" onClick={leaveRoom}>
-				Leave Room
-			</button>
-		</div>
-	);
+  return (
+    <div className="room">
+      You are in ROOM: {room.name}
+      <div className="participants">
+        {count === 2 ? <p id="prompt">{prompt}</p> : ""}
+
+        <Participant
+          isAnalysed1={isAnalysed1}
+          setAnalysed1={setAnalysed1}
+          isAnalysed2={isAnalysed2}
+          setAnalysed2={setAnalysed2}
+          key={room.localParticipant.identity}
+          localParticipant="true"
+          participant={room.localParticipant}
+          prompt={prompt}
+          count={count}
+          setCount={setCount}
+        />
+        {remoteParticipants.map((participant) => (
+          <Participant
+            key={participant.identity}
+            participant={participant}
+            isAnalysed1={isAnalysed1}
+            setAnalysed1={setAnalysed1}
+            isAnalysed2={isAnalysed2}
+            setAnalysed2={setAnalysed2}
+            count={count}
+            setCount={setCount}
+          />
+        ))}
+      </div>
+      <button id="leaveRoom" onClick={leaveRoom}>
+        Leave Room
+      </button>
+    </div>
+  );
 }
