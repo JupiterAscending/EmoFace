c2649bc7427ffb97593b43dc12459f31b45b012a
Yuriko Hijikata <akanesasu3@gmail.com>
2021-06-24 19:06:42 +0900

    fixing deployment error (removed twilio from server.js)

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
-const twilio = require("twilio");
+// const twilio = require("twilio");
 // const AccessToken = require('twilio').jwt.AccessToken;
 
 app.use(cors());
