44277614e1f466695c4d9a20073d96150bcd0a21
yuriko-coder <yurikohijikata@Yurikos-iMac.local>
2021-07-31 20:05:13 +0900

    added video-token route to server

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
-// const twilio = require("twilio");
-// const AccessToken = require('twilio').jwt.AccessToken;
+const twilio = require("twilio");
 
 app.use(cors());
 app.use(express.json());
@@ -13,25 +12,23 @@ app.use(express.static("build"));
 
 const port = process.env.PORT || 5000;
 
-// app.post("/video-token", (req, res)=>{
-//     const {identity, room} = req.body;
-//     console.log({identity, room})
-//     const AccessToken = twilio.jwt.AccessToken;
+app.post("/video-token", (req, res) => {
+  const { identity, room } = req.body;
+  const AccessToken = twilio.jwt.AccessToken;
 
-//     const VideoGrant = AccessToken.VideoGrant;
-//     const token = new AccessToken(
-//         process.env.TWILIO_ACCOUNT_SID,
-//         process.env.TWILIO_API_KEY,
-//         process.env.TWILIO_API_SECRET
-//       );
-//     token.identity = identity;
-//     const grant = new VideoGrant();
-//       grant.room = room;
-//       token.addGrant(grant);
+  const VideoGrant = AccessToken.VideoGrant;
+  const token = new AccessToken(
+    process.env.TWILIO_ACCOUNT_SID,
+    process.env.TWILIO_API_KEY,
+    process.env.TWILIO_API_SECRET
+  );
+  token.identity = identity;
+  const grant = new VideoGrant();
+  grant.room = room;
+  token.addGrant(grant);
 
-//       return token.toJwt()
-
-// })
+  return token.toJwt();
+});
 
 app.listen(port, () => {
   console.log(`Server is listening on ${port}`);
