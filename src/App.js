1aa34ab03be02ee4217d5c12f403cb71a918efef
Yuricoder <akanesasu3@gmail.com>
2021-08-01 22:29:06 +0900

    added realtime update to prompt, userscores

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import { database } from "./firebase";
 import VideoChat from "./videochat/VideoChat";
 
 export default function App() {
-  // useEffect(() => {
-  //   database.test.get().then((snapshot) => {
-  //     snapshot.docs.map((s) => console.log(s.data()));
-  //   });
-  // }, []);
+  useEffect(() => {
+    database.test.get().then((snapshot) => {
+      snapshot.docs.map((s) => console.log(s.data()));
+    });
+  }, []);
 
   // async function joinRoom() {
   //   try {
