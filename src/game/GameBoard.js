dfcbce630926245928917386d4d937a87a91f4d6
Miho Ogura <pluto04@live.jp>
2021-08-02 18:51:18 +0900

    Counter sync finished

--- src/game/GameBoard.js
+++ src/game/GameBoard.js
@@ -5,272 +5,296 @@ import Loader from "react-loader-spinner";
 import * as faceapi from "face-api.js";
 import { database } from "../firebase";
 
-function GameBoard({ room }) {
-  const [prompt, setPrompt] = useState(generatePrompt());
-  const [counter, setCounter] = useState(3);
-  const [showCounter, setShowCounter] = useState(false);
-  const [showScoreboard, setShowScoreboard] = useState(false);
-  const [isPlaying, setIsPlaying] = useState(false);
+function GameBoard ({ room }) {
+	const [ prompt, setPrompt ] = useState(generatePrompt());
+	const [ counter, setCounter ] = useState(3);
+	const [ showCounter, setShowCounter ] = useState(false);
+	const [ showScoreboard, setShowScoreboard ] = useState(false);
+	const [ isPlaying, setIsPlaying ] = useState(false);
 
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState("");
+	const [ loading, setLoading ] = useState(false);
+	const [ error, setError ] = useState("");
 
-  const [isCapture, setCapture] = useState(false);
-  const [users, setUsers] = useState([]);
+	const [ isCapture, setCapture ] = useState(false);
+	const [ users, setUsers ] = useState([]);
 
-  useEffect(() => {
-    // create a entry in a room database with your name
-    database.rooms
-      .doc(room.name)
-      .set(
-        {
-          prompt: prompt,
-          isPlaying: false,
-          users: [],
-        },
-        { merge: true }
-      )
-      .then(() => {
-        // realtime subscription
-        database.rooms.doc(room.name).onSnapshot((doc) => {
-          // sync prompt
-          const currentPrompt = doc.data().prompt;
-          console.log("Realtime update snapshot----- prompt", doc.data().prompt);
-          if (prompt) setPrompt(currentPrompt);
+	useEffect(() => {
+		// create a entry in a room database with your name
+		database.rooms
+			.doc(room.name)
+			.set(
+				{
+					prompt: prompt,
+					isPlaying: false,
+					users: [],
+					counter: counter,
+					showCounter: showCounter,
+				},
+				{ merge: true }
+			)
+			.then(() => {
+				// realtime subscription
+				database.rooms.doc(room.name).onSnapshot((doc) => {
+					// sync prompt
+					const currentPrompt = doc.data().prompt;
+					console.log("Realtime update snapshot----- prompt", doc.data().prompt);
+					if (prompt) setPrompt(currentPrompt);
 
-          // sync score & captured face
-          const users = doc.data().users;
-          console.log("Realtime users", users);
-          if (users.length > 0) {
-            setUsers(users);
+					// sync counter & score & captured face
+					const currentShowCounter = doc.data().showCounter;
+					console.log(currentShowCounter, "this is showCounter");
+					setShowCounter(currentShowCounter);
 
-            for (let user of users) {
-              drawCanvas(user, user.capturedFace);
-            }
-          }
-        });
-      });
-  }, []);
+					const currentCounter = doc.data().counter;
+					setCounter(currentCounter);
 
-  const countDown = () => {
-    return new Promise((resolve) => {
-      const id = setInterval(() => {
-        setCounter((counter) => {
-          if (counter > 0) {
-            return counter - 1;
-          } else {
-            setShowCounter(false);
-            clearInterval(id);
-            // reset counter
-            setCounter(3);
-            resolve();
-          }
-        });
-      }, 1000);
-    });
-  };
+					const users = doc.data().users;
+					console.log("Realtime users", users);
+					if (users.length > 0) {
+						setUsers(users);
 
-  const drawCanvas = (participant, base64) => {
-    if (base64 === "") return;
-    console.count("draw canvas was called!");
+						for (let user of users) {
+							drawCanvas(user, user.capturedFace);
+						}
+					}
+				});
+			});
+	}, []);
 
-    const canvas = document.getElementById(participant.identity + "-canvas");
-    const container = document.getElementById("canvas-container");
-    canvas.width = parseInt(container.clientWidth); //canvasの幅
-    canvas.height = parseInt(container.clientHeight);
-    console.log(canvas, "canvas this is in drawCanvas");
+	const countDown = async () => {
+		await database.rooms.doc(room.name).update(
+			{
+				showCounter: true,
+			},
+			{ merge: true }
+		);
 
-    const img = new Image();
-    img.src = base64;
-    console.log("IMG", img);
-    canvas.getContext("2d").drawImage(img, 0, 0, canvas.width, canvas.height);
-    console.log("canvas after draw", canvas);
-  };
+		console.log(showCounter);
 
-  const capture = async (participants) => {
-    for (let participant of participants) {
-      const canvas = document.getElementById(participant.identity + "-canvas");
-      const video = document.getElementById(participant.identity);
-      // console.log({ video });
-      const container = document.getElementById("canvas-container");
-      canvas.width = parseInt(container.clientWidth); //canvasの幅
-      canvas.height = parseInt(container.clientHeight);
-      canvas.getContext("2d").drawImage(video, 0, 0, canvas.width, canvas.height); //videoタグの「今」の状態をcanvasに描写
-      const imgData = canvas.toDataURL("image/jpeg");
-      participant.capturedFace = imgData;
-    }
-    // save to database
-    await database.rooms.doc(room.name).update(
-      {
-        users: participants,
-      },
-      { merge: true }
-    );
-  };
+		return new Promise((resolve) => {
+			const id = setInterval(() => {
+				setCounter((counter) => {
+					if (counter > 0) {
+						database.rooms.doc(room.name).update(
+							{
+								counter: counter - 1,
+							},
+							{ merge: true }
+						);
+						// return counter - 1;
+					} else {
+						database.rooms.doc(room.name).update(
+							{
+								counter: 3,
+								showCounter: false,
+							},
+							{ merge: true }
+						);
+						setShowCounter(false);
+						clearInterval(id);
+						// reset counter
+						setCounter(3);
 
-  const analyse = async (participants) => {
-    setError("");
-    setLoading(true);
-    for (let participant of participants) {
-      const canvas = document.getElementById(participant.identity + "-canvas");
+						resolve();
+					}
+				});
+			}, 1000);
+		});
+	};
 
-      await faceapi.nets.tinyFaceDetector.load("/models");
-      await faceapi.nets.faceExpressionNet.load("/models");
-      const detectionsWithExpressions = await faceapi
-        .detectAllFaces(canvas, new faceapi.TinyFaceDetectorOptions())
-        .withFaceExpressions();
+	const drawCanvas = (participant, base64) => {
+		if (base64 === "") return;
+		console.count("draw canvas was called!");
 
-      let score = calculateScore(detectionsWithExpressions);
-      if (score === undefined) {
-        score = 0;
-        setError("顔と認識されませんでした");
-      }
+		const canvas = document.getElementById(participant.identity + "-canvas");
+		const container = document.getElementById("canvas-container");
+		canvas.width = parseInt(container.clientWidth); //canvasの幅
+		canvas.height = parseInt(container.clientHeight);
+		console.log(canvas, "canvas this is in drawCanvas");
 
-      participant.score = score;
+		const img = new Image();
+		img.src = base64;
+		console.log("IMG", img);
+		canvas.getContext("2d").drawImage(img, 0, 0, canvas.width, canvas.height);
+		console.log("canvas after draw", canvas);
+	};
 
-      console.log(score);
-    }
-    // save score to database
-    console.log({ participants });
-    database.rooms.doc(room.name).update(
-      {
-        users: participants,
-      },
-      { merge: true }
-    );
+	const capture = async (participants) => {
+		for (let participant of participants) {
+			const canvas = document.getElementById(participant.identity + "-canvas");
+			const video = document.getElementById(participant.identity);
+			// console.log({ video });
+			const container = document.getElementById("canvas-container");
+			canvas.width = parseInt(container.clientWidth); //canvasの幅
+			canvas.height = parseInt(container.clientHeight);
+			canvas.getContext("2d").drawImage(video, 0, 0, canvas.width, canvas.height); //videoタグの「今」の状態をcanvasに描写
+			const imgData = canvas.toDataURL("image/jpeg");
+			participant.capturedFace = imgData;
+		}
+		// save to database
+		await database.rooms.doc(room.name).update(
+			{
+				users: participants,
+			},
+			{ merge: true }
+		);
+	};
 
-    //   if (score) {
-    //     database.scores.doc(room.name).set(
-    //       {
-    //         [participant.identity]: score,
-    //       },
-    //       { merge: true }
-    //     );
-    //   } else {
-    //     console.log("noscore");
-    //   }
-    // } else {
-    //   setAnalysed2(true);
-    //   setResult_p2(detectionsWithExpressions);
+	const analyse = async (participants) => {
+		setError("");
+		setLoading(true);
+		for (let participant of participants) {
+			const canvas = document.getElementById(participant.identity + "-canvas");
 
-    //   const score2 = calculateScore(detectionsWithExpressions);
-    //   console.log("score2-----", score2);
-    //   if (score2) {
-    //     database.scores.doc(room.name).set(
-    //       {
-    //         [participant.identity]: score2,
-    //       },
-    //       { merge: true }
-    //     );
-    //   } else {
-    //     console.log("no score!");
-    //   }
-    //   setAnalysed1(false);
-    //   setAnalysed2(false);
-    // }
-    setLoading(false);
-  };
+			await faceapi.nets.tinyFaceDetector.load("/models");
+			await faceapi.nets.faceExpressionNet.load("/models");
+			const detectionsWithExpressions = await faceapi
+				.detectAllFaces(canvas, new faceapi.TinyFaceDetectorOptions())
+				.withFaceExpressions();
 
-  const calculateScore = (detectionsWithExpressions) => {
-    if (detectionsWithExpressions[0]) {
-      console.log(prompt);
-      const float = parseFloat(detectionsWithExpressions[0].expressions[prompt]);
-      console.log(
-        "inside calculatescore----",
-        detectionsWithExpressions[0].expressions[prompt],
-        prompt
-      );
-      const multiplied = float * 100;
-      const score = multiplied.toFixed(2);
-      console.log({ float, multiplied, score });
-      return score;
-    } else {
-      setLoading(false);
-      setError("Please capture again! Please make sure to take a clear picture!");
-    }
-  };
-  const handleChangePrompt = () => {
-    const prompt = generatePrompt();
-    setPrompt(prompt);
-    savePrompt(prompt);
-  };
+			let score = calculateScore(detectionsWithExpressions);
+			if (score === undefined) {
+				score = 0;
+				setError("顔と認識されませんでした");
+			}
 
-  const savePrompt = async (prompt) => {
-    await database.rooms
-      .doc(room.name)
-      .update({
-        prompt: prompt,
-      })
-      .then(() => {
-        console.log("prompt updated!!", prompt);
-      });
-  };
+			participant.score = score;
 
-  const handleGameSet = async () => {
-    // 0. save prompt to the database
-    console.log({ prompt });
-    await savePrompt(prompt);
+			console.log(score);
+		}
+		// save score to database
+		console.log({ participants });
+		database.rooms.doc(room.name).update(
+			{
+				users: participants,
+			},
+			{ merge: true }
+		);
 
-    // 1. start a timer
-    setShowCounter(true);
-    await countDown();
+		//   if (score) {
+		//     database.scores.doc(room.name).set(
+		//       {
+		//         [participant.identity]: score,
+		//       },
+		//       { merge: true }
+		//     );
+		//   } else {
+		//     console.log("noscore");
+		//   }
+		// } else {
+		//   setAnalysed2(true);
+		//   setResult_p2(detectionsWithExpressions);
 
-    // 2. capture face
-    const localParticipant = { identity: room.localParticipant.identity, score: 0 };
-    const remoteParticipants = Array.from(room.participants).map((participant) => {
-      return { identity: participant[1].identity, score: 0 };
-    });
+		//   const score2 = calculateScore(detectionsWithExpressions);
+		//   console.log("score2-----", score2);
+		//   if (score2) {
+		//     database.scores.doc(room.name).set(
+		//       {
+		//         [participant.identity]: score2,
+		//       },
+		//       { merge: true }
+		//     );
+		//   } else {
+		//     console.log("no score!");
+		//   }
+		//   setAnalysed1(false);
+		//   setAnalysed2(false);
+		// }
+		setLoading(false);
+	};
 
-    setUsers([localParticipant, ...remoteParticipants]);
+	const calculateScore = (detectionsWithExpressions) => {
+		if (detectionsWithExpressions[0]) {
+			console.log(prompt);
+			const float = parseFloat(detectionsWithExpressions[0].expressions[prompt]);
+			console.log("inside calculatescore----", detectionsWithExpressions[0].expressions[prompt], prompt);
+			const multiplied = float * 100;
+			const score = multiplied.toFixed(2);
+			console.log({ float, multiplied, score });
+			return score;
+		} else {
+			setLoading(false);
+			setError("Please capture again! Please make sure to take a clear picture!");
+		}
+	};
+	const handleChangePrompt = () => {
+		const prompt = generatePrompt();
+		setPrompt(prompt);
+		savePrompt(prompt);
+	};
 
-    capture([localParticipant, ...remoteParticipants]);
+	const savePrompt = async (prompt) => {
+		await database.rooms
+			.doc(room.name)
+			.update({
+				prompt: prompt,
+			})
+			.then(() => {
+				console.log("prompt updated!!", prompt);
+			});
+	};
 
-    // 3. analyse
-    // analyse([localParticipant, ...remoteParticipants]);
-  };
+	const handleGameSet = async () => {
+		// 0. save prompt to the database
+		console.log({ prompt });
+		await savePrompt(prompt);
 
-  return (
-    <div>
-      <div class="mt-3 text-xl text-pink-300 ml-3 text-center md:text-2xl lg:text-3xl">
-        <span class="mb-2">
-          Make your {prompt} {faces[prompt]} face!
-        </span>
-        <br />
-        {/* {showScoreboard && (
+		// 1. start a timer
+		setShowCounter(true);
+		await countDown();
+
+		// 2. capture face
+		const localParticipant = { identity: room.localParticipant.identity, score: 0 };
+		const remoteParticipants = Array.from(room.participants).map((participant) => {
+			return { identity: participant[1].identity, score: 0 };
+		});
+
+		setUsers([ localParticipant, ...remoteParticipants ]);
+
+		capture([ localParticipant, ...remoteParticipants ]);
+
+		// 3. analyse
+		analyse([ localParticipant, ...remoteParticipants ]);
+	};
+
+	return (
+		<div>
+			<div class="mt-3 text-xl text-pink-300 ml-3 text-center md:text-2xl lg:text-3xl">
+				<span class="mb-2">
+					Make your {prompt} {faces[prompt]} face!
+				</span>
+				<br />
+				{/* {showScoreboard && (
           <span className="text-white mt-6">
             {room.localParticipant.identity}: %
             <br />
             "user2": %
           </span>
         )} */}
-      </div>
-      {showCounter && (
-        <div className="timer-container">
-          <p className="timer">{counter}</p>
-        </div>
-      )}
-      <button
-        class="w-40 lg:w-50 bg-pink-400 hover:bg-pink-700 text-white font-bold py-2 px-4 rounded-full mb-5 mt-5"
-        onClick={handleChangePrompt}
-      >
-        Change prompt?
-      </button>
-      <button
-        class="w-40 lg:w-50 bg-pink-400 hover:bg-pink-700 text-white font-bold py-2 px-4 rounded-full mb-5"
-        onClick={handleGameSet}
-      >
-        Game Set
-      </button>
-      {loading && (
-        <Loader type="Circles" color="rgb(244, 114, 182)" height={50} width={50} />
-      )}
-      <p class="text-yellow">{error}</p>
-      {users.map((participant) => (
-        <Canvas participant={participant} />
-      ))}
-      {/* <FaceCapture participants={users} /> */}
-    </div>
-  );
+			</div>
+			{showCounter && (
+				<div className="timer-container">
+					<p className="timer">{counter}</p>
+				</div>
+			)}
+			<button
+				class="w-40 lg:w-50 bg-pink-400 hover:bg-pink-700 text-white font-bold py-2 px-4 rounded-full mb-5 mt-5"
+				onClick={handleChangePrompt}
+			>
+				Change prompt?
+			</button>
+			<button
+				class="w-40 lg:w-50 bg-pink-400 hover:bg-pink-700 text-white font-bold py-2 px-4 rounded-full mb-5"
+				onClick={handleGameSet}
+			>
+				Game Set
+			</button>
+			{loading && <Loader type="Circles" color="rgb(244, 114, 182)" height={50} width={50} />}
+			<p class="text-white">{error}</p>
+			{users.map((participant) => <Canvas participant={participant} />)}
+			{/* <FaceCapture participants={users} /> */}
+		</div>
+	);
 }
 
 export default GameBoard;
--- src/videochat/Participant.js
+++ src/videochat/Participant.js
@@ -3,75 +3,82 @@ import * as canvas from "canvas";
 import * as faceapi from "face-api.js";
 import Loader from "react-loader-spinner";
 
-function Participant({ participant }) {
-  const [videoTracks, setVideoTracks] = useState([]);
-  const [audioTracks, setAudioTracks] = useState([]);
+function Participant ({ participant }) {
+	const [ videoTracks, setVideoTracks ] = useState([]);
+	const [ audioTracks, setAudioTracks ] = useState([]);
 
-  const videoRef = useRef();
-  const audioRef = useRef();
+	const videoRef = useRef();
+	const audioRef = useRef();
 
-  const trackpubsToTracks = (trackMap) =>
-    Array.from(trackMap.values())
-      .map((publication) => publication.track)
-      .filter((track) => track !== null);
+	const trackpubsToTracks = (trackMap) =>
+		Array.from(trackMap.values()).map((publication) => publication.track).filter((track) => track !== null);
 
-  useEffect(() => {
-    const trackSubscribed = (track) => {
-      if (track.kind === "video") {
-        setVideoTracks((videoTacks) => [...videoTacks, track]);
-      } else {
-        setAudioTracks((audioTracks) => [...audioTracks, track]);
-      }
-    };
+	useEffect(
+		() => {
+			const trackSubscribed = (track) => {
+				if (track.kind === "video") {
+					setVideoTracks((videoTacks) => [ ...videoTacks, track ]);
+				} else {
+					setAudioTracks((audioTracks) => [ ...audioTracks, track ]);
+				}
+			};
 
-    const trackUnsubscribed = (track) => {
-      if (track.kind === "video") {
-        setVideoTracks((videoTracks) => videoTracks.filter((v) => v !== track));
-      } else {
-        setAudioTracks((audioTracks) => audioTracks.filter((a) => a !== track));
-      }
-    };
+			const trackUnsubscribed = (track) => {
+				if (track.kind === "video") {
+					setVideoTracks((videoTracks) => videoTracks.filter((v) => v !== track));
+				} else {
+					setAudioTracks((audioTracks) => audioTracks.filter((a) => a !== track));
+				}
+			};
 
-    setVideoTracks(trackpubsToTracks(participant.videoTracks));
-    setAudioTracks(trackpubsToTracks(participant.audioTracks));
+			setVideoTracks(trackpubsToTracks(participant.videoTracks));
+			setAudioTracks(trackpubsToTracks(participant.audioTracks));
 
-    participant.on("trackSubscribed", trackSubscribed);
-    participant.on("trackUnsubscribed", trackUnsubscribed);
+			participant.on("trackSubscribed", trackSubscribed);
+			participant.on("trackUnsubscribed", trackUnsubscribed);
 
-    return () => {
-      setVideoTracks([]);
-      setAudioTracks([]);
-      participant.removeAllListeners();
-    };
-  }, [participant]);
+			return () => {
+				setVideoTracks([]);
+				setAudioTracks([]);
+				participant.removeAllListeners();
+			};
+		},
+		[ participant ]
+	);
 
-  useEffect(() => {
-    const videoTrack = videoTracks[0];
-    if (videoTrack) {
-      videoTrack.attach(videoRef.current);
-      return () => {
-        console.log("video track detaching");
-        videoTrack.detach();
-      };
-    }
-  }, [videoTracks]);
-  useEffect(() => {
-    const audioTrack = audioTracks[0];
-    if (audioTrack) {
-      audioTrack.attach(audioRef.current);
-      return () => {
-        audioTrack.detach();
-      };
-    }
-  }, [audioTracks]);
+	useEffect(
+		() => {
+			const videoTrack = videoTracks[0];
+			if (videoTrack) {
+				videoTrack.attach(videoRef.current);
+				return () => {
+					console.log("video track detaching");
+					videoTrack.detach();
+				};
+			}
+		},
+		[ videoTracks ]
+	);
+	useEffect(
+		() => {
+			const audioTrack = audioTracks[0];
+			if (audioTrack) {
+				audioTrack.attach(audioRef.current);
+				return () => {
+					audioTrack.detach();
+				};
+			}
+		},
+		[ audioTracks ]
+	);
 
-  return (
-    <div className="participant">
-      <h3>{participant.identity}</h3>
-      <video ref={videoRef} autoPlay={true} id={participant.identity}></video>
-      <audio ref={audioRef} autoPlay={true} muted={true}></audio>
-    </div>
-  );
+	return (
+		<div className="participant">
+			<h3 class="text-white">{participant.identity.toUpperCase()}</h3>
+			<video ref={videoRef} autoPlay={true} id={participant.identity} />
+			<audio ref={audioRef} autoPlay={true} muted={true} />
+		</div>
+	);
 }
 
 export default Participant;
