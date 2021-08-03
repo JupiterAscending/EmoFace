ee36808edfe577aad2adb79bc6a7e5ed2d7c127b
Yuricoder <akanesasu3@gmail.com>
2021-08-03 21:51:43 +0900

    moved the position of onboarding button

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ import OnBoarding from "./OnBoarding";
 export default function App() {
   const [showOnboarding, setShowOnboarding] = useState(false);
   return (
-    <main className="bg-blue-900 flex justify-center main">
-      <div>
+    <main className="bg-blue-900 flex justify-center ">
+      <div class="mt-10">
         <h1 className="text-5xl text-white text-center lg:text-7xl mb-5">EmoFace 🤪</h1>
         <div className="text-white text-center">
           <button
-            className="hover:text-pink-700"
+            className="hover:text-pink-700 fixed top-0 right-0"
             onClick={() => {
               setShowOnboarding(!showOnboarding);
             }}
